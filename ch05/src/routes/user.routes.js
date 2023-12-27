f97a197a66c7289a4169f1acfef09cf31aa5f339
Just2Deep <deepakrajsrs@gmail.com>
2023-12-27 09:20:26 +0530

    add channel profile and watch history endpoints
    add user routes profile and history

--- ch05/src/routes/user.routes.js
+++ ch05/src/routes/user.routes.js
@@ -2,6 +2,8 @@ import { Router } from "express";
 import {
     changeCurrentPassword,
     getCurrentUser,
+    getUserChannelProfile,
+    getWatchHistory,
     loginUser,
     logoutUser,
     refreshAccessToken,
@@ -37,11 +39,15 @@ router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
 router.route("/me").get(verifyJWT, getCurrentUser);
 router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+
 router
     .route("/update-avatar")
     .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
 router
     .route("/update-cover")
     .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;
