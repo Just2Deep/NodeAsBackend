ab2d012aa547fe533bdfb28738d0cc8d294f9faf
Just2Deep <deepakrajsrs@gmail.com>
2023-12-29 09:18:25 +0530

    add remaining models

---
+++ ch05/src/models/comments.model.js
@@ -0,0 +1,22 @@
+import mongoose, { Schema } from "mongoose";
+
+const commentSchema = new Schema(
+    {
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+        },
+        video: {
+            type: Schema.Types.ObjectId,
+            ref: "Video",
+        },
+        content: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+    },
+    { timestamps: true }
+);
+
+export const Comment = mongoose.model("Comment", commentSchema);
