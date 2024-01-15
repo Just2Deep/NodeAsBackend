e9137cfbb0e6e5b92c9639d2bef24c491b6484f9
Just2Deep <deepakrajsrs@gmail.com>
2024-01-15 19:37:29 +0530

    completed like route

--- ch05/src/controllers/like.controller.js
+++ ch05/src/controllers/like.controller.js
@@ -6,21 +6,149 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const { videoId } = req.params;
-    //TODO: toggle like on video
+
+    if (!(videoId && isValidObjectId(videoId))) {
+        throw new ApiError(400, "invalid channel id");
+    }
+
+    const isLiked = await Like.findOne({
+        video: videoId,
+        likedBy: req.user?._id,
+    });
+
+    if (isLiked) {
+        await Like.findOneAndDelete({
+            video: videoId,
+            likedBy: req.user?._id,
+        });
+
+        return res
+            .status(200)
+            .json(new ApiResponse(200, {}, "unliked video successfully!"));
+    }
+
+    const like = await Like.create({
+        video: videoId,
+        likedBy: req.user?._id,
+    });
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, like, "liked video successfully!"));
 });
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
     const { commentId } = req.params;
-    //TODO: toggle like on comment
+
+    if (!(commentId && isValidObjectId(commentId))) {
+        throw new ApiError(400, "invalid channel id");
+    }
+
+    const isLiked = await Like.findOne({
+        comment: commentId,
+        likedBy: req.user?._id,
+    });
+
+    if (isLiked) {
+        await Like.findOneAndDelete({
+            comment: commentId,
+            likedBy: req.user?._id,
+        });
+
+        return res
+            .status(200)
+            .json(new ApiResponse(200, {}, "unliked comment successfully!"));
+    }
+
+    const like = await Like.create({
+        comment: commentId,
+        likedBy: req.user?._id,
+    });
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, like, "liked comment successfully!"));
 });
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
     const { tweetId } = req.params;
-    //TODO: toggle like on tweet
+
+    if (!(tweetId && isValidObjectId(tweetId))) {
+        throw new ApiError(400, "invalid channel id");
+    }
+
+    const isLiked = await Like.findOne({
+        tweet: tweetId,
+        likedBy: req.user?._id,
+    });
+
+    if (isLiked) {
+        await Like.findOneAndDelete({
+            tweet: tweetId,
+            likedBy: req.user?._id,
+        });
+
+        return res
+            .status(200)
+            .json(new ApiResponse(200, {}, "unliked tweet successfully!"));
+    }
+
+    const like = await Like.create({
+        tweet: tweetId,
+        likedBy: req.user?._id,
+    });
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, like, "liked tweet successfully!"));
 });
 
 const getLikedVideos = asyncHandler(async (req, res) => {
-    //TODO: get all liked videos
+    // get all liked videos by current user
+
+    const videosList = await Like.aggregate([
+        {
+            $match: {
+                likedBy: new mongoose.Types.ObjectId(req.user?._id),
+                video: { $exists: true },
+            },
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "video",
+                foreignField: "_id",
+                as: "video",
+                pipeline: [
+                    {
+                        $project: {
+                            videoFile: 1,
+                            thumbnail: 1,
+                            title: 1,
+                            description: 1,
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            $addFields: {
+                video: {
+                    $first: "$video",
+                },
+            },
+        },
+    ]);
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                videosList,
+                "liked videos fetched successfully!"
+            )
+        );
 });
 
 export { toggleCommentLike, toggleTweetLike, toggleVideoLike, getLikedVideos };
