43f49d11b7cf086b8ff0963178b3a5c1fb18ec6d
Just2Deep <deepakrajsrs@gmail.com>
2024-01-20 10:43:14 +0530

    completed dashboard

--- ch05/src/app.js
+++ ch05/src/app.js
@@ -33,7 +33,7 @@ import videoRouter from "./routes/video.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import likeRouter from "./routes/like.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
-// import dashboardRouter from "./routes/dashboard.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
 
 //routes declaration
 app.use("/api/v1/users", userRouter);
@@ -44,7 +44,7 @@ app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/healthcheck", healthcheckRouter);
 app.use("/api/v1/playlists", playlistRouter);
-// app.use("/api/v1/dashboard", dashboardRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
 
 // http://localhost:8000/api/v1/users/register
 
